920874ebb8aacaccbf44825fec2168088f57a437
YJ Lin <0xff07@gmail.com>
2020-02-26 00:19:10 +0800

    Implement everything except sort

--- queue.c
+++ queue.c
@@ -1,9 +1,10 @@
+#include "queue.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "harness.h"
-#include "queue.h"
 
 /*
  * Create empty queue.
@@ -12,17 +13,36 @@
 queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
-    /* TODO: What if malloc returned NULL? */
+    if (!q)
+        goto oom_q;
+
     q->head = NULL;
+    q->tail = NULL;
+    q->size = 0;
     return q;
+
+oom_q:
+    return NULL;
 }
 
 /* Free all storage used by queue */
 void q_free(queue_t *q)
 {
+    if (!q)
+        goto null_q;
     /* TODO: How about freeing the list elements and the strings? */
     /* Free queue structure */
+    list_ele_t *ptr = q->head;
+    while (ptr) {
+        list_ele_t *tmp = ptr->next;
+        free(ptr->value);
+        free(ptr);
+        ptr = tmp;
+    }
     free(q);
+
+null_q:
+    return;
 }
 
 /*
@@ -34,14 +54,35 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
+    if (!q)
+        goto null_q;
+
     list_ele_t *newh;
-    /* TODO: What should you do if the q is NULL? */
     newh = malloc(sizeof(list_ele_t));
-    /* Don't forget to allocate space for the string and copy it */
-    /* What if either call to malloc returns NULL? */
+    if (!newh)
+        goto oom_node;
+
+    int len = strlen(s);
+    char *_s = malloc(sizeof(char) * (len + 1));
+    if (!_s)
+        goto oom_string;
+
+    strncpy(_s, s, len + 1);
+    newh->value = _s;
     newh->next = q->head;
+
     q->head = newh;
+    if (!q->size) {
+        q->tail = newh;
+    }
+    q->size++;
     return true;
+
+oom_string:
+    free(newh);
+oom_node:
+null_q:
+    return false;
 }
 
 /*
@@ -56,6 +97,37 @@ bool q_insert_tail(queue_t *q, char *s)
     /* TODO: You need to write the complete code for this function */
     /* Remember: It should operate in O(1) time */
     /* TODO: Remove the above comment when you are about to implement. */
+    if (!q)
+        goto null_q;
+
+    list_ele_t *newh;
+    newh = malloc(sizeof(list_ele_t));
+    if (!newh)
+        goto oom_node;
+
+    int len = strlen(s);
+    char *_s = malloc(sizeof(char) * (len + 1));
+    if (!_s)
+        goto oom_string;
+
+    strncpy(_s, s, len + 1);
+    newh->value = _s;
+    newh->next = NULL;
+
+    if (!q->tail) {
+        q->tail = newh;
+        q->head = newh;
+    } else {
+        q->tail->next = newh;
+        q->tail = newh;
+    }
+    q->size++;
+    return true;
+
+oom_string:
+    free(newh);
+oom_node:
+null_q:
     return false;
 }
 
@@ -71,8 +143,28 @@ bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
 {
     /* TODO: You need to fix up this code. */
     /* TODO: Remove the above comment when you are about to implement. */
+    if (!q)
+        goto null_q;
+
+    if (!q->head)
+        goto empty_q;
+
+    list_ele_t *tmp = q->head;
     q->head = q->head->next;
+
+    if (sp) {
+        strncpy(sp, tmp->value, bufsize);
+        sp[bufsize - 1] = 0;
+    }
+
+    free(tmp->value);
+    free(tmp);
+    q->size--;
     return true;
+
+empty_q:
+null_q:
+    return false;
 }
 
 /*
@@ -84,6 +176,12 @@ int q_size(queue_t *q)
     /* TODO: You need to write the code for this function */
     /* Remember: It should operate in O(1) time */
     /* TODO: Remove the above comment when you are about to implement. */
+    if (!q)
+        goto null_q;
+
+    return q->size;
+
+null_q:
     return 0;
 }
 
@@ -96,8 +194,31 @@ int q_size(queue_t *q)
  */
 void q_reverse(queue_t *q)
 {
+    if (!q)
+        goto null_q;
+
+    if (!q->size)
+        goto empty_q;
+
+    q->tail = q->head;
+
+    list_ele_t *head_new = NULL, *head_old = NULL;
+    head_new = q->head;
+    head_old = q->head->next;
+    head_new->next = NULL;
+    while (head_old) {
+        list_ele_t *tmp = head_old->next;
+        head_old->next = head_new;
+        head_new = head_old;
+        head_old = tmp;
+    }
+    q->head = head_new;
+    return;
     /* TODO: You need to write the code for this function */
     /* TODO: Remove the above comment when you are about to implement. */
+empty_q:
+null_q:
+    return;
 }
 
 /*
